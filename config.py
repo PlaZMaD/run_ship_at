c15bee88968dfd7dc1bc0b62855d890c837286f2
Igor <igor.musinov@phystech.edu>
2020-01-21 18:02:58 +0300

    config for Kuber job
    + ignore IDEA

---
+++ config.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# parameters of server
+K8S_PROXY = 'http://127.0.0.1:8002'
+
+HOST_OUTPUT_DIRECTORY = '/mnt/shipfs'
+DOCKER_OUTPUT_DIRECTORY = '/output'
+
+HOST_SAMPLE_DIRECTORY = '/local/ship/background_2018'
+DOCKER_SAMPLE_DIRECTORY = '/sample'
+
+
+JOB_SPEC = {
+    "apiVersion": "batch/v1",
+    "kind": "Job",
+    "metadata": {
+        "name": "EKship-job-{}"
+    },
+    "spec": {
+        # Don't forget about this disabled option
+        # "ttlSecondsAfterFinished": 14400,
+        "template": {
+            "spec": {
+                "containers": [
+                    {
+                        "name": "EKship",
+                        "image": "mrphys/mfsimage:ff_for_tests",
+                        "env": [
+                            {"name": "fileName",
+                             "value": "pythia8_Geant4_10.0_withCharmandBeauty0_mu.root"},
+                            {"name": "mfirstEvent",
+                             "value": "0"},
+                            {"name": "nEvents",
+                             "value": "10"},
+                            {"name": "muShieldDesign",
+                             "value": "9"},
+                            {"name": "jName",
+                             "value": "testJob"},
+                            {"name": "jNumber",
+                             "value": "1"},
+                        ],
+                        "resources": {
+                            "requests": {
+                                "memory": "6Gi",
+                                "cpu": "1"
+                            },
+                            "limits": {
+                                "memory": "6Gi",
+                                "cpu": "1"
+                            }
+                        },
+                        "volumeMounts": [
+                            {
+                                "mountPath": DOCKER_OUTPUT_DIRECTORY,
+                                "name": "output"
+                            },
+                            {
+                                "mountPath": DOCKER_SAMPLE_DIRECTORY,
+                                "name": "muonsample"
+                            }
+                        ]
+                    }
+                ],
+                "restartPolicy": "Never",
+                "volumes": [
+                    {
+                        "name": "output",
+                        "hostPath": {
+                            "path": "{}/{}",
+                            "type": "Directory"
+                        }
+                    },
+                    {
+                        "name": "muonsample",
+                        "hostPath": {
+                            "path": HOST_SAMPLE_DIRECTORY,
+                            "type": "Directory"
+                        }
+                    }
+                ]
+            }
+        },
+        "backoffLimit": 1
+    }
+}
