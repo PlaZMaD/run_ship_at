8686ee7efdec1617f01a9c872ac1b29324bf7583
Igor <igor.musinov@phystech.edu>
2020-03-28 16:22:55 +0300

    if not empty

--- run_jobs.sh
+++ run_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #mkdir -p /output/$jName/$fileName/$jNumber
-if [ -z "$AZURE_INPUT_DATA_URI" ]
+if [ ! -z "$AZURE_INPUT_DATA_URI" ]
 then
     echo "AZURE_DATA_URI is given. Starting downloading data from $AZURE_INPUT_DATA_URI..."
     azcopy cp "$AZURE_INPUT_DATA_URI" "/sample" --recursive
@@ -14,7 +14,7 @@ alienv -w /sw setenv  FairShip/latest  -c /bin/bash -c "python \$FAIRSHIP/macro/
 mv ship.conical.MuonBack-TGeant4_rec.root /tmp/$jName/$fileName/$jNumber/
 cp /tmp/$jName/$fileName/$jNumber/*  /output #/$jName/$fileName/$jNumber/
 
-if [ -z "$AZURE_OUTPUT_DATA_URI" ]
+if [ ! -z "$AZURE_OUTPUT_DATA_URI" ]
 then
     echo "Starting uploading results to $AZURE_OUTPUT_DATA_URI..."
     azcopy cp "/tmp/$jName/$fileName/$jNumber/*" "$AZURE_OUTPUT_DATA_URI" --recursive
